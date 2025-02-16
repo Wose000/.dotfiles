03ccb31759404340ab6f8722955f5e93415e5530
wose <jiro.due@gmail.com>
2025-02-16 08:35:15 +0100

    fast push

--- bin/bin/pen_display_setup.sh
+++ bin/bin/pen_display_setup.sh
@@ -2,14 +2,20 @@
 
 DEVICE_NAME="Tablet Monitor Pen Pen"
 
-DISPLAY="DP-3"
+OUTPUT="DP-3"
+
+export DISPLAY=:0
+export XAUTHORITY=/home/wose/.Xauthority
 
 udevadm monitor --subsystem-match=input | while read -r line; do
 	if xinput list | grep -i "$DEVICE_NAME"; then
 		DEVICE_ID=$(xinput list | grep -i "$DEVICE_NAME" | grep -o 'id=[0-9]*' | cut -d= -f2)
-		if [ -n "$DISPLAY" ]; then
-			xinput map-to-output "$DEVICE_ID" "$DISPLAY"
+		if [ -n "$DEVICE_ID" ]; then
+			echo "$DEVICE_ID"
+			xinput map-to-output "$DEVICE_ID" "$OUTPUT"
+			killall udevadm
 			break
 		fi
 	fi
 done
+
