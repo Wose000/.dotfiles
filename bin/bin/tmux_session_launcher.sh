be2a8a081bca1156fb2f94cd2f8a9deacf330e9b
wose <jiro.due@gmail.com>
2025-06-05 07:58:04 +0200

    fast update

--- bin/bin/tmux_session_launcher.sh
+++ bin/bin/tmux_session_launcher.sh
@@ -1,43 +1,46 @@
-#!/bin/bash
+#!/bin/env bash
 
 attach_or_switch() {
-	if [ -z "$TMUX" ]; then
-		# not in tmux
-		tmux attach-session -t $1
-	else
-		# inside tmux
-		tmux switch-client -t $1
-	fi
+    if [ -z "$TMUX" ]; then
+        # not in tmux
+        tmux attach-session -t $1
+    else
+        # inside tmux
+        tmux switch-client -t $1
+    fi
 }
 
 get_folder_name() {
-  path_without_trailing_slash="${1%/}"
-  echo "${path_without_trailing_slash##*/}"
+    path_without_trailing_slash="${1%/}"
+    basename="${path_without_trailing_slash##*/}"
+    echo "${basename//./}"
 }
 
 sessions_folders=(
-  "$HOME/m/prj/"
-  "$HOME/m/src/"
-  "$HOME/.dotfiles/"
+    "$HOME/m/prj/"
+    "$HOME/m/src/"
+    "$HOME/.dotfiles/"
 )
 
 sessions_path=(
-  "$HOME"
-  "$HOME/m/doc/note/"
-  "$HOME/.dotfiles/"
-  )
+    "$HOME"
+    "$HOME/m/doc/note/"
+    "$HOME/.dotfiles/"
+    "$HOME/m/pathfinder/pathfinder_vault/"
+)
 
-session_folder="$( { fd -d 1 -t d . "${sessions_folders[@]}"; printf '%s\n' "${sessions_path[@]}"; }| fzf)"
+selected_folder="$( { fd -d 1 -t d . "${sessions_folders[@]}"; printf '%s\n' "${sessions_path[@]}"; }| fzf)"
 
-echo "$session_folder"
+if [[ -z $selected_folder ]]; then
+    echo "No solection"
+    exit 0
+fi
 
-session_name="$(get_folder_name "$session_folder")"
+session_name="$(get_folder_name "$selected_folder")"
 
-echo "$session_name"
-tmux has-session -t $session_name 2>/dev/null
-
-if [ $? != 0 ]; then
-	tmux new-session -d -s $session_name -c $session_folder -n 'Nvim'
+if ! tmux has-session -t $session_name 2>/dev/null; then
+    echo ok
+    tmux new-session -d -s "$session_name" -c "$selected_folder" -n 'Nvim'
 fi
 
 attach_or_switch "$session_name"
