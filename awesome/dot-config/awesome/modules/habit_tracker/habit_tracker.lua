5de36d34d0267849e0d181532fe339f579aec3e6
wose <jiro.due@gmail.com>
2025-06-06 22:43:53 +0200

    fast update

--- awesome/dot-config/awesome/modules/habit_tracker/habit_tracker.lua
+++ awesome/dot-config/awesome/modules/habit_tracker/habit_tracker.lua
@@ -1,17 +1,52 @@
 local json = require("dkjson")
+local wibox = require("wibox.init")
+local beautiful = require("beautiful")
 
-local file = io.open("./data/habits.json", "r")
+local M = {}
 
-local habits_table = {}
-if file then
-	habits_table = file:read("*a")
-	file:close()
+local function get_habits_data()
+	local file = io.open("/home/wose/.dotfiles/awesome/dot-config/awesome/modules/habit_tracker/data/habits.json", "r")
+	local habits_table = {}
+	if file then
+		habits_table = file:read("*a")
+		file:close()
+	end
+	local data, _, err = json.decode(habits_table)
+	return data
 end
 
-local data, pos, err = json.decode(habits_table)
-
-if err then
-	print("Error", err)
-else
-	print(data[1].title)
+local function get_habit_widget(data)
+	local succes_rate = data.successes / (data.successes + data.fails)
+	local w = {
+		{
+			text = data.title,
+			widget = wibox.widget.textbox,
+		},
+		{
+			value = succes_rate,
+			forced_height = 10,
+			forced_width = 100,
+			color = beautiful.bg_focus,
+			background_color = beautiful.bg_normal,
+			widget = wibox.widget.progressbar,
+		},
+		layout = wibox.layout.fixed.vertical,
+		margins = 4,
+		widget = wibox.container.margin,
+	}
+	return w
 end
+
+function M.get_habits_widgets()
+	local habits = get_habits_data()
+	local w = {}
+	for i, habit in ipairs(habits) do
+		table.insert(w, get_habit_widget(habit))
+	end
+	w.layout = wibox.layout.fixed.vertical
+	w.margins = 4
+	w.widget = wibox.container.margin
+	return w
+end
+
+return M
--- awesome/dot-config/awesome/modules/habit_tracker/ui.lua
+++ awesome/dot-config/awesome/modules/habit_tracker/ui.lua
@@ -2,6 +2,8 @@ local wibox = require("wibox.init")
 local awful = require("awful")
 local gears = require("gears")
 
+local habits_fun = require("modules.habit_tracker.habit_tracker")
+
 local bar_icon = wibox.widget({
 	widget = wibox.widget.textbox,
 	text = "H",
@@ -10,17 +12,11 @@ local bar_icon = wibox.widget({
 })
 
 local popup = awful.popup({
-	widget = {
-		{
-			text = "Habits",
-			widget = wibox.widget.textbox,
-		},
-		margins = 10,
-		widget = wibox.container.margin,
-	},
+	screen = screen[1],
+	widget = habits_fun.get_habits_widgets(),
 	border_color = "#000000",
 	border_width = 2,
-	placement = awful.placement.centered,
+	placement = awful.placement.top_right,
 	ontop = true,
 	visible = false,
 	shape = gears.shape.rounded_rect,
