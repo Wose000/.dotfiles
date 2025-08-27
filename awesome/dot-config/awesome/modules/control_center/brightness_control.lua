be20a14ef1050fb9a31451fcc81ec121a412081c
Wose000 <jiro.due@gmail.com>
2025-08-27 20:03:01 +0200

    fast update

--- awesome/dot-config/awesome/modules/control_center/brightness_control.lua
+++ awesome/dot-config/awesome/modules/control_center/brightness_control.lua
@@ -2,6 +2,29 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local awful = require("awful")
 local gears = require("gears")
+local naughty = require("naughty")
+
+local max_brightness_file = "/sys/class/backlight/intel_backlight/max_brightness"
+local max_brightness_value = -1
+
+awful.spawn.easy_async_with_shell("cat" .. max_brightness_file, function(stout, sterr, _, exit_code)
+	if exit_code == 0 then
+		local max_brightness = tonumber(stout)
+		if max_brightness then
+			max_brightness_value = max_brightness
+		else
+			naughty.notification({
+				title = "error",
+				message = "error converting to a number to output of: cat " .. max_brightness_file,
+			})
+		end
+	else
+		naughty.notification({
+			title = "error",
+			message = "Error reading the file " .. max_brightness_file .. " " .. sterr,
+		})
+	end
+end)
 
 local widget = wibox.widget({
 	bar_shape = gears.shape.rounded_rect,
@@ -11,7 +34,7 @@ local widget = wibox.widget({
 	handle_shape = gears.shape.circle,
 	handle_border_color = beautiful.border_normal,
 	handle_border_width = 1,
-	value = 25,
+	value = max_brightness_value,
 	widget = wibox.widget.slider,
 })
 
