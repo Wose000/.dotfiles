5eab7c8fbfde99d6d6eebff25a6b744458990165
Wose000 <jiro.due@gmail.com>
2025-08-31 08:25:37 +0200

    fast update

--- awesome/dot-config/awesome/modules/control_center/brightness_control.lua
+++ awesome/dot-config/awesome/modules/control_center/brightness_control.lua
@@ -5,19 +5,28 @@ local gears = require("gears")
 local naughty = require("naughty")
 
 local max_brightness_file = "/sys/class/backlight/intel_backlight/max_brightness"
+local brightness_file = "/sys/class/backlight/intel_backlight/brightness"
 local brightness_script = "/home/wose/bin/brightness"
 
 local brightness_control = {}
+
+local function calc_percentage_of_brightness(p)
+	local decimal_percentage = p / 100
+	local brightness_val = math.floor((brightness_control.max_brightness * decimal_percentage) + 0.5)
+	if brightness_val >= brightness_control.max_brightness then
+		return brightness_control.max_brightness
+	end
+	if brightness_val <= 0 then
+		return 1
+	end
+	return brightness_val
+end
 
 -- Set max brightness reading it from the max_brightness system file
 awful.spawn.easy_async_with_shell("cat " .. max_brightness_file, function(stout, sterr, _, exit_code)
 	if exit_code == 0 then
 		local max_brightness = tonumber(stout)
 		if max_brightness then
-			naughty.notification({
-				title = "error",
-				message = "max  brightness " .. max_brightness,
-			})
 			brightness_control.max_brightness = max_brightness
 		else
 			naughty.notification({
@@ -32,6 +41,25 @@ awful.spawn.easy_async_with_shell("cat " .. max_brightness_file, function(stout,
 			message = "Error reading the file " .. max_brightness_file .. " " .. sterr,
 		})
 		error("Error executing the script exit code: " .. exit_code .. "\nError: " .. sterr)
+	end
+end)
+
+awful.spawn.easy_async_with_shell("cat " .. brightness_file, function(stout, sterr, _, exit_code)
+	if exit_code == 0 then
+		local brightness = tonumber(stout)
+		if brightness then
+			brightness_control.brightness = brightness
+		else
+			naughty.notification({
+				title = "error",
+				message = "error converting to a number to output of: cat " .. brightness_file,
+			})
+		end
+	else
+		naughty.notification({
+			title = "error",
+			message = "Error reading the file " .. brightness_file .. " " .. sterr,
+		})
 	end
 end)
 
@@ -58,22 +86,17 @@ brightness_control.widget = wibox.widget({
 	forced_height = 20,
 })
 
-local function calc_percentage_of_brightness(p)
-	local decimal_percentage = p / 100
-	local brightness_val = math.floor((brightness_control.max_brightness * decimal_percentage) + 0.5)
-	if brightness_val >= brightness_control.max_brightness then
-		return brightness_control.max_brightness
-	end
-	if brightness_val <= 0 then
-		return 1
-	end
-	return brightness_val
-end
+--- max_brightness : 100 = brightness : x
 
 -- Connect to `property::value` to use the value on change
 slider:connect_signal("property::value", function(_, new_value)
 	local new_brightness = calc_percentage_of_brightness(new_value)
 	awful.spawn.easy_async_with_shell("sudo " .. brightness_script .. " " .. new_brightness, function() end)
 end)
+
+local function percentage_out_of_value(val)
+	local tmp = val * 100 / brightness_control.max_brightness
+	return math.floor(tmp + 0.5)
+end
 
 return brightness_control
