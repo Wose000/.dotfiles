e874db98ad852931bbef0ab9128a69e65e0ab255
wose <jiro.due@gmail.com>
2025-09-30 09:23:23 +0200

    fast update

--- bin/bin/fun.sh
+++ bin/bin/fun.sh
@@ -1,12 +1,20 @@
 #!/bin/env bash
 
 gfp(){
-	git add .
-	git commit -m "fast push"
-	git push origin main
+    git add .
+    git commit -m "fast push"
+    git push origin main
 }
 
 mod() {
 
-	rclone --vfs-cache-mode writes mount "onedrive": ~/mnt/onedrive/
+    rclone --vfs-cache-mode writes mount "onedrive": ~/mnt/onedrive/
+}
+
+function y() {
+    local tmp="$(mktemp  -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d '' cwd < "$tmp"
+    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+    rm -f -- "$tmp"
 }
