d3b13af3843c1e575d1b0dca8e6544b14d8b66ca
Wose000 <jiro.due@gmail.com>
2025-10-23 13:45:45 +0200

    fast update

--- awesome/dot-config/awesome/modules/signal/network.lua
+++ awesome/dot-config/awesome/modules/signal/network.lua
@@ -1,11 +1,13 @@
 local awful = require("awful")
 local gears = require("gears")
+local helpers = require("modules.utils.helpers")
 
+-- returns 0 on stdout if there is internet connection
 local cmd = "ping -c 1 google.com>&/dev/null; echo $?"
 
 local status = {
-	CONNECTED = 1,
-	DISCONNECTED = 2,
+	CONNECTED = "connected",
+	DISCONNECTED = "disconnected",
 }
 
 local current_status = status.DISCONNECTED
@@ -13,17 +15,18 @@ local current_status = status.DISCONNECTED
 local function check_internet_status()
 	awful.spawn.easy_async_with_shell(cmd, function(stdout, stderr, reason, exit_code)
 		if exit_code == 0 then
-			local new_status = status.CONNECTED
-			if new_status ~= current_status then
+			local new_status = ""
+			if tonumber(stdout) == 0 then
+				new_status = status.CONNECTED
+			else
+				new_status = status.DISCONNECTED
+			end
+			if new_status == "" or new_status ~= current_status then
+				awesome.emit_signal("internet::" .. new_status)
 				current_status = new_status
-				awesome.emit_signal("internet::connected")
 			end
 		else
-			local new_status = status.DISCONNECTED
-			if new_status ~= current_status then
-				current_status = new_status
-				awesome.emit_signal("internet::disconnected")
-			end
+			helpers.debug_log(string.format("Error, exit_code: %d", exit_code))
 		end
 	end)
 end
