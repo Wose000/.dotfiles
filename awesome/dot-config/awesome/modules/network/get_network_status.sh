3cef8c03435df446400de4a2cf47fbdf9481d4ba
wose <jiro.due@gmail.com>
2025-11-21 08:29:10 +0100

    fast update

--- awesome/dot-config/awesome/modules/network/get_network_status.sh
+++ awesome/dot-config/awesome/modules/network/get_network_status.sh
@@ -4,12 +4,15 @@ regex() {
     [[ $1 =~ $2 ]] && printf '%s\n' "${BASH_REMATCH[1]}"
 }
 
-ETHERNET_INTERFACE="enp2s0"
+ETHERNET_INTERFACE="enp"
+WIRELESS_INTERFACE="wps"
 
-inet_partial=$(ip -brief addr | grep "$ETHERNET_INTERFACE")
+ethernet_partial=$(ip -brief addr | grep "$ETHERNET_INTERFACE")
+wireless_partial=$(ip -brief addr | grep "$WIRELESS_INTERFACE")
 
-inet=$(regex "$inet_partial" '^enp2s0\s*UP\s*([0-9]{3}\.[0-9]{3}\.[0-9]\.[0-9]{2}).*$')
+inet_wired=$(regex "$ethernet_partial" '^enp[a-zA-Z0-9]*\s*UP\s*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*$')
+inet_wireless=$(regex "$wireless_partial" '^wps[a-zA-Z0-9]*\s*UP\s*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*$')
 
 public=$(curl -s ipinfo.io/ip)
 
-echo -e "$inet $public"
+echo -e "inet_wired:$inet_wired public:$public"
