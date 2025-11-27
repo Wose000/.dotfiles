4d0b9b857c63b8b68bc041294c45aa7fde7a2c92
wose <jiro.due@gmail.com>
2025-11-27 10:23:58 +0100

    fast update

--- awesome/dot-config/awesome/modules/control_center/control_button.lua
+++ awesome/dot-config/awesome/modules/control_center/control_button.lua
@@ -4,42 +4,69 @@ local beautiful = require("beautiful")
 local awful = require("awful")
 local gears = require("gears")
 
-local states = {
+---@enum states
+local STATES = {
 	unselected = 0,
 	selected = 1,
 }
 
-local M = {
-	icon = "󰂯",
-	state = states.unselected,
-	on_select_function = nil,
-}
+---Base class to generate control buttons
+---@class ControlButton
+---@field icon string icon for the button
+---@field on_selected_function fun():nil | nil to be called on button selected
+---@field on_released_function fun():nil | nil to be called on button deselected
+---@field state states # button state, unselected = 0, selected = 1
+local ControlButton = {}
 
-local background = wibox.widget({
-	shape = gears.shape.squircle,
-	bg = beautiful.inactive,
-	widget = wibox.container.background,
-})
+ControlButton.__index = ControlButton
 
-background:connect_signal("mouse::enter", function()
-	background.bg = beautiful.fg_normal
-end)
+---Constructor for ControlButton
+---@param icon string Icon that's going to be displayed on the button
+---@param on_selected_function fun():nil | nil callback for on_button_selected
+---@param on_released_function fun():nil | nil callback for on_button_released
+---@return ControlButton # returns a new istance of control button
+function ControlButton.new(icon, on_selected_function, on_released_function)
+	---@class ControlButton
+	local self = setmetatable({}, ControlButton)
 
-background:connect_signal("mouse::leave", function()
-	if M.state == states.unselected then
-		background.bg = beautiful.inactive
-	else
-		background.bg = beautiful.accent
-	end
-end)
+	self.icon = icon
 
-function M.get_button()
+	self.on_select_callback = on_selected_function
+
+	self.on_release_callback = on_released_function
+
+	self.state = STATES.unselected
+
+	return self
+end
+
+---Return the button
+---@this ControlButton
+function ControlButton:get_button()
+	local background = wibox.widget({
+		shape = gears.shape.squircle,
+		bg = beautiful.inactive,
+		widget = wibox.container.background,
+	})
+
+	background:connect_signal("mouse::enter", function()
+		background.bg = beautiful.fg_normal
+	end)
+
+	background:connect_signal("mouse::leave", function()
+		if self.state == STATES.unselected then
+			background.bg = beautiful.inactive
+		else
+			background.bg = beautiful.accent
+		end
+	end)
+
 	local button = wibox.widget({
 		{
 			{
 				{
 					{
-						markup = M.icon,
+						markup = self.icon,
 						widget = wibox.widget.textbox,
 						valign = "center",
 						halign = "center",
@@ -60,11 +87,17 @@ function M.get_button()
 	})
 
 	button:buttons(awful.button({}, 1, function()
-		if M.state == states.unselected then
-			M.state = states.selected
+		if self.state == STATES.unselected then
+			self.state = STATES.selected
+			if self.on_select_callback then
+				self.on_select_callback()
+			end
 			background.bg = beautiful.accent
 		else
-			M.state = states.unselected
+			self.state = STATES.unselected
+			if self.on_release_callback then
+				self.on_release_callback()
+			end
 			background.bg = beautiful.inactive
 		end
 	end))
@@ -72,4 +105,4 @@ function M.get_button()
 	return button
 end
 
-return M
+return ControlButton
