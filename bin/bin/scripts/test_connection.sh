5a9d9f69cbb6e739b4ff1a550f40d16ac807d641
wose <jiro.due@gmail.com>
2026-01-13 18:49:49 +0100

    fast update

--- bin/bin/scripts/test_connection.sh
+++ bin/bin/scripts/test_connection.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 if ping -c 1 -W 2 8.8.8.8 >&/dev/null; then
-  $HOME/bin/uall
+  echo "Internet up, update dotfiles"
 else
   echo "No internet connection available skip updates.."
 fi
