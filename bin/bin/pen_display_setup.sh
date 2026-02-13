0a5180e6aad2f01f2a9c2d1a6466c294bb74c643
wose <jiro.due@gmail.com>
2026-02-13 20:47:40 +0100

    fast update

--- bin/bin/pen_display_setup.sh
+++ bin/bin/pen_display_setup.sh
@@ -7,9 +7,15 @@ OUTPUT="DP-3"
 export DISPLAY=:0
 export XAUTHORITY=/home/wose/.Xauthority
 
+if [[ $HOSTNAME != "arch-pc" ]]; then
+  notify-send "this aint the desktop"
+  exit 1
+fi
+
 while true; do
     DEVICE_ID=$(xinput list | grep -i "$DEVICE_NAME" | grep -o 'id=[0-9]*' | cut -d= -f2)
     if [ -n "$DEVICE_ID" ]; then
+        notify-send "pen set on the correct display"
         xinput map-to-output "$DEVICE_ID" "$OUTPUT"
         break
     fi
