01368d4d7449851c1b52b0724c825ed52f26b544
wose <jiro.due@gmail.com>
2026-02-25 08:55:30 +0100

    fast update

---
+++ qutebrowser/dot-config/qutebrowser/config.py
@@ -0,0 +1,28 @@
+# pyright: reportUndefinedVariable=false, reportUnknownMemberType=false
+config.load_autoconfig()
+# Setting Dark Mode
+config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.preferred_color_scheme", "dark")
+
+c.fonts.completion.category = "JetBrainMono Nerd Font Mono"
+c.fonts.completion.entry = "JetBrainMono Nerd Font Mono"
+c.fonts.contextmenu = "JetBrainMono Nerd Font Mono"
+c.fonts.debug_console = "JetBrainMono Nerd Font Mono"
+c.fonts.default_family = "JetBrainMono Nerd Font Mono"
+c.fonts.downloads = "JetBrainMono Nerd Font Mono"
+c.fonts.hints = "JetBrainMono Nerd Font Mono"
+c.fonts.keyhint = "JetBrainMono Nerd Font Mono"
+c.fonts.messages.error = "JetBrainMono Nerd Font Mono"
+c.fonts.messages.info = "JetBrainMono Nerd Font Mono"
+c.fonts.messages.warning = "JetBrainMono Nerd Font Mono"
+c.fonts.prompts = "JetBrainMono Nerd Font Mono"
+c.fonts.statusbar = "JetBrainMono Nerd Font Mono"
+c.fonts.tabs.selected = "JetBrainMono Nerd Font Mono"
+c.fonts.tabs.unselected = "JetBrainMono Nerd Font Mono"
+c.fonts.tooltip = "JetBrainMono Nerd Font Mono"
+c.fonts.web.family.cursive = "JetBrainMono Nerd Font Mono"
+c.fonts.web.family.fantasy = "JetBrainMono Nerd Font Mono"
+c.fonts.web.family.fixed = "JetBrainMono Nerd Font Mono"
+c.fonts.web.family.sans_serif = "JetBrainMono Nerd Font Mono"
+c.fonts.web.family.serif = "JetBrainMono Nerd Font Mono"
+c.fonts.web.family.standard = "JetBrainMono Nerd Font Mono"
