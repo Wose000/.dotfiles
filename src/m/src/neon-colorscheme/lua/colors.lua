224ba6392d82969009d07177f85cc3cd4d6f6aaa
wose <jiro.due@gmail.com>
2026-05-13 20:00:02 +0200

    fast update

---
+++ src/m/src/neon-colorscheme/lua/colors.lua
@@ -0,0 +1,25 @@
+--- S = 100
+--- L = 64
+local M = {}
+
+M.colors = {
+	black = "#090a13",
+	bright_black = "#0f101c",
+	black_blue = "#191826",
+	grey = "#8d8fa7",
+	white = "#b7bad9",
+	white_blue = "#9afff9",
+	green = "#00dc79",
+	teal = "#00d8b6",
+	dark_teal = "#00afb7",
+	blue = "#00a3f3",
+	dark_blue = "#6f9bff",
+	purple = "#a38dff",
+	violet = "#c280ff",
+	magenta = "#df68ff",
+	pink = "#ff61c8",
+	peach = "#ff668e",
+	yellow = "#ddd66e",
+}
+
+return M
